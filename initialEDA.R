1f33ceb73f09e6b642cb4e20130c450b8ac2754d
sophiamir <smir@gwu.edu>
2022-03-28 09:34:38 -0400

    test

--- initialEDA.R
+++ initialEDA.R
@@ -121,7 +121,7 @@ prop.table(two_way1,1)#row percentages
 #During the day, the most frequent crime is theft for auto. The most frequent crime in the evening is theft for other things except autos. In the midnight, the most frequent crime is
 #theft for other things except autos.
 
-<<<<<<< Updated upstream
+
 #What types of offenses are more common at different times of year?
   
 
